82be9f76d2dbae7880122ab7bb093dd6ea0c7937
William Brodie-Tyrrell <william@brodie-tyrrell.org>
2016-04-25 11:02:38 +0930

    transfer from svn

---
+++ DS1620.cpp
@@ -0,0 +1,119 @@
+/* -*- C++ -*- */
+
+#include "DS1620.h"
+#include <Arduino.h>
+
+DS1620::DS1620(char dq, char clk, char rst)
+    : DQ(dq), CLK(clk), RST(rst)
+{
+    pinMode(DQ, INPUT);
+    digitalWrite(CLK, 1);
+    digitalWrite(RST, 0);
+    pinMode(RST, OUTPUT);
+    pinMode(CLK, OUTPUT);
+}
+
+void DS1620::raw_write(short c, char bits)
+{
+  pinMode(DQ, OUTPUT);  
+  digitalWrite(RST, 1);
+  
+  for(int i=0;i<bits;++i){
+    digitalWrite(CLK, 0);
+    digitalWrite(DQ, c & 1);
+    digitalWrite(CLK, 1);
+    c >>= 1;
+  }  
+}
+
+void DS1620::write(char addr, short value, int bits)
+{
+  digitalWrite(CLK, 1);
+  digitalWrite(RST, 0);
+  raw_write(addr);
+  raw_write(value, bits);
+  digitalWrite(RST, 0);
+}
+
+short DS1620::read(char addr, char bits)
+{
+  digitalWrite(CLK, 1);
+  digitalWrite(RST, 0);
+  raw_write(addr);
+
+  pinMode(DQ, INPUT);
+
+  short result=0;
+
+  for(char i=0;i<bits;++i){
+    digitalWrite(CLK, 0);
+    // result >>= 1;
+    // result |= digitalRead(DQ) << (bits-1);
+    result |= short(digitalRead(DQ)) << i;
+    digitalWrite(CLK, 1);
+  }
+  
+  digitalWrite(RST, 0);
+  
+  return result;
+}
+
+void DS1620::setTL(short tl)
+{
+    wait_write();
+    write(WRITE_TL, tl);        
+}
+
+short DS1620::getTL()
+{
+    return read(READ_TL);
+}
+
+void DS1620::setTH(short th)
+{
+    wait_write();
+    write(WRITE_TH, th);
+}
+
+short DS1620::getTH()
+{
+    return read(READ_TH);
+}
+
+void DS1620::setConfig(unsigned char c)
+{
+    wait_write();
+    write(WRITE_CONF, c, 8);
+}
+
+unsigned char DS1620::getConfig()
+{
+    return (unsigned char) (read(READ_CONF, 8) & 0xFF);
+}
+    
+void DS1620::convert()
+{
+    write(CONVERT, 0, 0);
+}
+
+short DS1620::getTemp()
+{
+    return read(READ_TEMP);
+}
+
+bool DS1620::done()
+{
+    return getConfig() & DONE;
+}
+
+bool DS1620::nv_busy()
+{
+    return getConfig() & NVB;
+}
+
+void DS1620::wait_write()
+{    
+    for(int del=0;del<WRITE_TIMEOUT && nv_busy();++del){
+        delay(1);
+    }
+}
---
+++ DS1620.h
@@ -0,0 +1,70 @@
+/* -*- C++ -*- */
+
+#ifndef _DS1620_WIBT_
+#define _DS1620_WIBT_
+
+class DS1620 {
+public:
+
+    DS1620(char dq, char clk, char rst);
+
+    /// set temp-low threshold
+    void setTL(short tl);
+    /// get temp-low threshold
+    short getTL();
+    /// set temp-high threshold
+    void setTH(short th);
+    /// get temp-high threshold
+    short getTH();
+    /// set config byte
+    void setConfig(unsigned char c);
+    /// get config byte
+    unsigned char getConfig();
+    
+    /// initiate a conversion; required in one-shot mode
+    void convert();
+    /// get last conversion
+    short getTemp();
+    /// is conversion complete?
+    bool done();
+
+    // bits in the config byte
+    static const char DONE=0x80;
+    static const char THF=0x40;
+    static const char TLF=0x20;
+    static const char NVB=0x10;
+    static const char CBIT=0x08;
+    static const char CPU=0x02;
+    static const char SHOT1=0x01;
+
+private:
+
+    /// write to a register
+    void write(char addr, short value, int bits=9);
+    /// read from a register
+    short read(char addr, char bits=9);
+    /// raw writing of bits
+    void raw_write(short c, char bits=8);
+    /// wait until it's 15ms since last call - last flash writes complete
+    void wait_write();
+    /// is the non-volatile ram busy?
+    bool nv_busy();
+
+    const char DQ, CLK, RST;
+
+    static const int CONVERT_TIMEOUT=15;
+    static const short INVALID_TEMP=-1000;
+    static const int WRITE_TIMEOUT=20;
+
+    static const char READ_TEMP=0xAA;
+    static const char WRITE_TH=0x01;
+    static const char WRITE_TL=0x02;
+    static const char READ_TH=0xA1;
+    static const char READ_TL=0xA2;
+    static const char CONVERT=0xEE;
+    static const char WRITE_CONF=0x0C;
+    static const char READ_CONF=0xAC;
+
+};
+
+#endif
